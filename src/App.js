a5803946c30e6a5109e66630dd0e14d951c3d1a8
ana <vane2013sanchez@gmail.com>
2024-09-07 11:32:41 -0300

    update-background

--- src/App.js
+++ src/App.js
@@ -1,38 +1,47 @@
-import { makeStyles } from "@material-ui/core";
+import React from 'react';
+import { makeStyles, CssBaseline } from "@material-ui/core";
 import Navbar from "./components/Navbar";
 import MyWork from './components/MyWork';
 import Contact from './components/Contact';
-import About from './components/About'
+import About from './components/About';
 import Skills from "./components/Skills";
-import ContactForm from './components/ContactForm'
-import Services from './components/Services'
+import ContactForm from './components/ContactForm';
+import Services from './components/Services';
 
 function App() {
-
-
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-
-      <Navbar></Navbar>
-      <About title="Inicio" id="about"/>
-      <Skills title="Sobre Mi" id="skill" dark={"#00BFA5"}/>
-     
-      <MyWork title="Trabajos" id="work" dark={false}/>
-       <Services title="Servicios" id="service" dark={true}/>
-      <ContactForm title="Contactos" id="contact" dark={true}/>
-      <Contact  />
-    </div>
+    <>
+      <CssBaseline /> {/* Normaliza los estilos y asegura una base consistente */}
+      <div className={classes.root}>
+        <Navbar />
+        <About title="Inicio" id="about" />
+        <Skills title="Sobre Mi" id="skill" dark={"#00BFA5"} />
+        <MyWork title="Trabajos" id="work" dark={false} />
+        <Services title="Servicios" id="service" dark={true} />
+        <ContactForm title="Contactos" id="contact" dark={true} />
+        <Contact />
+      </div>
+    </>
   );
 }
 
-
 const useStyles = makeStyles((theme) => ({
   root: {
-backgroundColor:'#1E2A38'
-  }
-
-}))
+    backgroundColor: '#1E2A38',
+    minHeight: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+    margin: 0,
+    padding: 0,
+    [theme.breakpoints.down('xs')]: {
+      // Estilos específicos para dispositivos móviles
+      backgroundColor: '#1E2A38',
+      margin: 0,
+      padding: 0,
+    },
+  },
+}));
 
 export default App;
